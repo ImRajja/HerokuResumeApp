7ba632fbeef045a4b28621debdaf8b87245f8a73
Raja <37906074+ImRajja@users.noreply.github.com>
2022-04-16 08:34:25 +0530

    -

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,11 +1,11 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
   noTodos: {
     minHeight: "67vh",
   },
   homeContainer: {
-    backgroundColor: "gray",
+    backgroundColor: "#f2f2f2",
     width: "100%",
   },
 }));
